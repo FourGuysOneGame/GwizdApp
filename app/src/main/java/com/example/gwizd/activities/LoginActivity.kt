d9f8b7e1c23255fd67b54dc149ef6cb06796ae03
Kamil <kamil.deren1@gmail.com>
2023-09-30 23:42:02 +0200

    Minor changes Still MAPS_API_KEY needed

--- app/src/main/java/com/example/gwizd/activities/LoginActivity.kt
+++ app/src/main/java/com/example/gwizd/activities/LoginActivity.kt
@@ -1,4 +1,5 @@
 package com.example.gwizd.activities
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +13,8 @@ class LoginActivity : ComponentActivity() {
     private lateinit var username: EditText
     private lateinit var password: EditText
     private lateinit var loginButton: Button
+    private lateinit var loginviaGoogleButton: Button
+    private lateinit var signupButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginActivityBinding.inflate(layoutInflater)
@@ -19,11 +22,20 @@ class LoginActivity : ComponentActivity() {
 
         binding.loginButton.setOnClickListener {
             if(binding.username.text.toString() == "user" && binding.password.text.toString() == "password") {
-                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
             }
             else {
                 Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
             }
+        }
+        binding.loginviaGoogleButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.signupButton.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/gwizd/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/gwizd/activities/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gwizd.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -13,6 +14,7 @@ class SignUpActivity : ComponentActivity() {
     private lateinit var username: EditText
     private lateinit var email: EditText
     private lateinit var password: EditText
+    private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SignupActivityBinding.inflate(layoutInflater)
@@ -23,6 +25,11 @@ class SignUpActivity : ComponentActivity() {
             email = binding.email
             password = binding.password
             Toast.makeText(this, "Sign Up failed", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
